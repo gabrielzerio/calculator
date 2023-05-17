e4db5447f4d051e76b63e1bf13fb07f975a58160
Gabriel <21043296+gabrielzerio@users.noreply.github.com>
2023-05-17 15:52:25 -0300

    interface is ok

--- script.js
+++ script.js
@@ -6,12 +6,13 @@ botoes.style.display = 'grid';
 document.addEventListener('DOMContentLoaded', () => {
     gridDisplay();
 });
-
+const letrasBotoes = ['(',')','%','AC','7','8','9','÷','4','5','6','x','1','2','3','-','0','.','=','+'];
 function gridDisplay(){
     botoes.style.gridTemplateColumns = 'repeat(4, 1fr)';
-botoes.style.gridTemplateRows = 'repeat(5, 1fr)';
+    botoes.style.gridTemplateRows = 'repeat(5, 1fr)';
     for(let i=0;i<20;i++){
-        let botao = document.createElement(`div${i}`);
+        let botao = document.createElement("div");
+        botao.textContent = letrasBotoes[i];
         botoes.appendChild(botao);
     }
 }
